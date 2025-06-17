5e7fd8f6df9e3c6cfb24c8e6e0a75947c8561a33
Developer <dev@example.com>
2025-06-17 18:27:39 +0700

    Add simple HTML version and update build config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        format: 'umd',
+        name: 'ReactApp',
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
